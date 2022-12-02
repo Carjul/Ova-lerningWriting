d43c2c96960b7b653c73c0180b20fe6632842a6a
Carlos Ramos González <emugameplay.tv@gmail.com>
2022-12-02 13:40:16 +0000

    sdds

--- src/public/my-app/src/components/actividades.jsx
+++ src/public/my-app/src/components/actividades.jsx
@@ -1,15 +1,16 @@
 import Nav from './nav';
-export default function Actividades() {
+export default function Actividades() { 
     return (
         <section>
             <Nav />
             <div className="contenedor">
 
                 <div className='uno'>
-                    <p>actividades aqui</p>
+                    <p>EDUCAPLAY</p>
+                    <iframe title='crucigrama' width="795" height="690" frameborder="0" src="https://es.educaplay.com/juego/5385887-english_writing_a1.html"></iframe>
                 </div>
                 <div className='dos'>
-                    <p>actividades dd</p>
+                    <p>H5P</p>
                 </div>
             </div>
         </section>
