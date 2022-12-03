6698dd97f9248ae5d3d93aca5668375f0caeca3a
Carlos <Carlosrgonzalez88@gmail.com>
2022-12-03 00:02:47 -0500

    ds

--- src/public/my-app/src/components/actividades.jsx
+++ src/public/my-app/src/components/actividades.jsx
@@ -6,7 +6,7 @@ export default function Actividades() {
             <div className="contenedor">
 
                 <div className='uno'>
-                    <p>EDUCAPLAY</p>
+                   
                     <iframe title='crucigrama' width="795" height="690" frameborder="0" src="https://es.educaplay.com/juego/5385887-english_writing_a1.html"></iframe>
                 </div>
                 <div className='dos'>
--- src/public/my-app/src/components/home.jsx
+++ src/public/my-app/src/components/home.jsx
@@ -9,10 +9,19 @@ export default function Home() {
             <div className="contenedor">
 
                 <div className='uno'>
-                    <p>home aqui</p>
+                    <h3>Writing</h3>
+                    <p>Es la capacidad de producir un texto coherente y relevante. La ortografía y las estructuras gramaticales son los mayores obstáculos a superar, aunque supone un reto menor que la escritura porque da más tiempo a pensar y, en ocasiones, permite usar un diccionario. Practicamos esta habilidad cuando escribimos mensajes, notas o textos.
+
+</p>
                 </div>
                 <div className='dos'>
-                    <p>home dd</p>
+                    <p><strong>Cómo practicar:</strong>
+
+                        Comunicarse por escrito en inglés con frecuencia (con amistades, profesores…) y hacer apuntes personales (listas de la compra o de tareas) en inglés.
+                        Llevar un diario o hacer redacciones que pueda corregir una persona especializada.
+                        Copiar expresiones útiles de libros o revistas para estudiar y aprenderlas.</p>
+                        
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/1HkeuMw7PyM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
              
